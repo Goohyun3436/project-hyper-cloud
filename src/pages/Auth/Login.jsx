143ef5a92e23f5bcb82c31b006766bba44289747
Goohyun3436 <rngus3436@g.kmou.ac.kr>
2022-10-15 06:00:59 +0900

    로그인, 회원가입 UI 완료

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,8 +1,21 @@
 import styled from 'styled-components';
-import LoginWrapper from './AuthWrapper';
+import AuthWrapper from '../../components/Auth/AuthWrapper';
+import AuthContent from '../../components/Auth/AuthContent';
+import InputWithLable from '../../components/Auth/InputWithLable';
+import AuthButton from '../../components/Auth/AuthButton';
+import BottomLink from '../../components/Auth/BottomLink';
 
 const Login = () => {
-  return <LoginWrapper>Login</LoginWrapper>;
+  return (
+    <AuthWrapper>
+      <AuthContent title='로그인'>
+        <InputWithLable label='이메일' name='email' placeholder='이메일' />
+        <InputWithLable label='비밀번호' name='password' placeholder='비밀번호' />
+        <AuthButton>로그인</AuthButton>
+        <BottomLink to='/auth/signup'>회원가입</BottomLink>
+      </AuthContent>
+    </AuthWrapper>
+  );
 };
 
 export default Login;
--- src/pages/Auth/Signup.jsx
+++ src/pages/Auth/Signup.jsx
@@ -1,8 +1,23 @@
 import styled from 'styled-components';
-import LoginWrapper from './AuthWrapper';
+import AuthWrapper from '../../components/Auth/AuthWrapper';
+import AuthContent from '../../components/Auth/AuthContent';
+import InputWithLable from '../../components/Auth/InputWithLable';
+import AuthButton from '../../components/Auth/AuthButton';
+import BottomLink from '../../components/Auth/BottomLink';
 
 const Signup = () => {
-  return <LoginWrapper>Login</LoginWrapper>;
+  return (
+    <AuthWrapper>
+      <AuthContent title='회원가입'>
+        <InputWithLable label='이메일' name='email' placeholder='이메일' />
+        <InputWithLable label='아이디' name='id' placeholder='아이디' />
+        <InputWithLable label='비밀번호' name='password' placeholder='비밀번호' />
+        <InputWithLable label='비밀번호 확인' name='passwordConfirm' placeholder='비밀번호 확인' />
+        <AuthButton>회원가입</AuthButton>
+        <BottomLink to='/auth/login'>로그인</BottomLink>
+      </AuthContent>
+    </AuthWrapper>
+  );
 };
 
 export default Signup;
