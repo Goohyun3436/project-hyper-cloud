5ece0a0a62fd6d7472c31062485d353a2b9d54d5
Goohyun3436 <rngus3436@g.kmou.ac.kr>
2022-10-15 06:36:13 +0900

    Add: 헤더에 ABOUT, XR BUSINESS, NEWS link 추가

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,13 @@
 import HeaderWrapper from './HeaderWrapper';
-import LoginButton from './LoginButton/LoginButton';
+import LoginButton from './contents/LoginButton';
+import NavItem from './contents/NavItem';
 
 const Header = () => {
   return (
     <HeaderWrapper>
+      <NavItem to='/about'>ABOUT</NavItem>
+      <NavItem to='/xr-business'>XR BUSINESS</NavItem>
+      <NavItem to='/news'>NEWS</NavItem>
       <LoginButton />
     </HeaderWrapper>
   );
