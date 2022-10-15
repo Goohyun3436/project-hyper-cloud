a4445b2ca0021492e217298f39c19ecef82efa3e
Goohyun3436 <rngus3436@g.kmou.ac.kr>
2022-10-16 02:40:07 +0900

    Add: 회원가입 email, userid 존재 유무 확인을 위한 redux 준비

--- src/reducers/auth.jsx
+++ src/reducers/auth.jsx
@@ -1,11 +1,17 @@
 import { Map } from 'immutable';
+import * as AuthAPI from '../lib/api/auth';
+
 const CHANGE_INPUT = 'auth/CHANGE_INPUT';
 const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
 const SET_ERROR = 'auth/SET_ERROR';
+const CHECK_EMAIL_EXISTS = 'auth/CHECK_EMAIL_EXISTS';
+const CHECK_USERID_EXISTS = 'auth/CHECK_USERID_EXISTS';
 
 export const changeInput = () => ({ type: CHANGE_INPUT });
 export const initializeForm = () => ({ type: INITIALIZE_FORM });
 export const setError = () => ({ type: SET_ERROR });
+export const checkEmailExists = () => ({ type: CHECK_EMAIL_EXISTS });
+export const checkUseridExists = () => ({ type: CHECK_USERID_EXISTS });
 
 const initialState = Map({
   signup: Map({
@@ -15,12 +21,20 @@ const initialState = Map({
       password: '',
       passwordConfirm: '',
     }),
+    exists: Map({
+      email: false,
+      password: false,
+    }),
     error: null,
   }),
   login: Map({
     form: Map({
       email: '',
       password: '',
+    }),
+    exists: Map({
+      email: false,
+      password: false,
     }),
     error: null,
   }),
@@ -41,6 +55,16 @@ const auth = (state = initialState, action) => {
     case SET_ERROR: {
       const { form, message } = action;
       return state.setIn([form, 'error'], message);
+    }
+
+    case CHECK_EMAIL_EXISTS: {
+      const { exists } = action;
+      return state.setIn(['register', 'exists', 'email'], exists);
+    }
+
+    case CHECK_USERID_EXISTS: {
+      const { exists } = action;
+      return state.setIn(['register', 'exists', 'userid'], exists);
     }
 
     default:
