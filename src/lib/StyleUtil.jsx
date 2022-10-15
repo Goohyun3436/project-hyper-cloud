563d08d6420d5dc63bb86b94dc4b04c573b26e4c
Goohyun3436 <rngus3436@g.kmou.ac.kr>
2022-10-16 00:11:40 +0900

    Add: 로그인, 회원가입 유효성 에러시 흔들림 효과 스타일 준비

--- src/lib/StyleUtil.jsx
+++ src/lib/StyleUtil.jsx
@@ -37,3 +37,23 @@ export const shadow = weight => {
 
   return shadows[weight];
 };
+
+export const transitions = {
+  shake: keyframes`
+      0% {
+          transform: translate(-30px);
+      }
+      25% {
+          transform: translate(15px);
+      }
+      50% {
+          transform: translate(-10px);
+      }
+      75% {
+          transform: translate(5px);
+      }
+      100% {
+          transform: translate(0px);
+      }
+  `,
+};
